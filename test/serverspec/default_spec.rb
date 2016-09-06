cf42cf8e132d2ebd1277d8b466856e02e8f22ae1
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2016-09-06 12:24:52 +0900

    add spec

--- test/serverspec/default_spec.rb
+++ test/serverspec/default_spec.rb
@@ -4,46 +4,42 @@ require 'serverspec'
 package = 'npppd'
 service = 'npppd'
 config  = '/etc/npppd/npppd.conf'
-user    = 'npppd'
-group   = 'npppd'
-ports   = [ PORTS ]
-log_dir = '/var/log/npppd'
-db_dir  = '/var/lib/npppd'
-
-case os[:family]
-when 'freebsd'
-  config = '/usr/local/etc/npppd.conf'
-  db_dir = '/var/db/npppd'
-end
-
-describe package(package) do
-  it { should be_installed }
-end 
+user    = '_npppd'
+group   = '_npppd'
+ports   = [ 1701 ]
+users_file  = '/etc/npppd/npppd-users'
 
 describe file(config) do
   it { should be_file }
-  its(:content) { should match Regexp.escape('npppd') }
+  its(:content) { should match /^tunnel l2tp_tunnel protocol l2tp \{\n\s+listen on 10\.0\.2\.15\n\}$/ }
+  # tunnel l2tp_tunnel protocol l2tp {
+  #   listen on 10.0.2.15
+  # }
+  # 
+  its(:content) { should match /^ipcp ipcp1 \{\n\s+pool-address 192\.168\.100\.1-192\.168\.100\.250\n\s+dns-servers 8\.8\.8\.8\n\}$/ }
+  # ipcp ipcp1 {
+  #   pool-address 192.168.100.1-192.168.100.250
+  #   dns-servers 8.8.8.8
+  # }
+  # 
+  its(:content) { should match /^interface pppx0 address 192\.168\.100\.254 ipcp ipcp1$/ }
+  # interface pppx0 address 192.168.100.254 ipcp ipcp1
+  # 
+  its(:content) { should match /^authentication LOCAL type local \{\n\s+users-file "\/etc\/npppd\/npppd-users"\n\}$/ }
+  # authentication LOCAL type local {
+  #   users-file "/etc/npppd/npppd-users"
+  # }
+  its(:content) { should match /^bind tunnel from l2tp_tunnel authenticated by LOCAL to pppx0$/ }
+  # bind tunnel from l2tp_tunnel authenticated by LOCAL to pppx0
+
 end
 
-describe file(log_dir) do
-  it { should exist }
-  it { should be_mode 755 }
-  it { should be_owned_by user }
-  it { should be_grouped_into group }
-end
+describe file(users_file) do
+  it { should be_file }
+  its(:content) { should match /^foo:\s+\\\n\s+:password=password\\c\\\\\\\^:\\/ }
+  its(:content) { should match /bar-:\s+\\\n\s+:password=password:\\\n\s+:framed-ip-address=192\.168\.100\.1:\\$/ }
+  its(:content) { should match /buz_:\s+\\\n\s+:password=password:\\\n\s+:framed-ip-network=192\.168\.101\.0\/24:$/ }
 
-describe file(db_dir) do
-  it { should exist }
-  it { should be_mode 755 }
-  it { should be_owned_by user }
-  it { should be_grouped_into group }
-end
-
-case os[:family]
-when 'freebsd'
-  describe file('/etc/rc.conf.d/npppd') do
-    it { should be_file }
-  end
 end
 
 describe service(service) do
@@ -53,6 +49,9 @@ end
 
 ports.each do |p|
   describe port(p) do
-    it { should be_listening }
+    it do
+      pending('serverspec does not work with netstat in OpenBSD')
+      should be_listening
+    end
   end
 end
