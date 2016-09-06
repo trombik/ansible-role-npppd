a9f007bd28211c1ae8a42dbf64ec032ddc3900cc
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2016-09-06 19:59:43 +0900

    update pf.conf

--- test/integration/server_client/provisioners/shell/gw1.sh
+++ test/integration/server_client/provisioners/shell/gw1.sh
@@ -8,15 +8,19 @@ cat > /etc/pf.conf <<'__EOF__'
 ext_if = "em1"
 int_if = "em2"
 vagrant_if = "em0"
+
+set skip on { lo, enc }
 block log all
-pass in on $vagrant_if proto tcp from any to ($vagrant_if) port ssh
-pass in on $ext_if proto tcp from any to ($ext_if) port ssh
-pass in on $int_if proto tcp from any to ($int_if) port ssh
-pass in on $ext_if from any to ($ext_if)
-pass in on $int_if from ($int_if:network) to any tag NAT
+pass in quick proto tcp from any to { ($ext_if), ($int_if), ($vagrant_if) } port ssh
+pass in quick on $ext_if from any to ($ext_if)
+pass in quick on $int_if from ($int_if:network) to any tag NAT
 match out on $ext_if from ($ext_if:network) to any nat-to ($ext_if) tagged NAT
-pass out tagged NAT
-pass out on $ext_if from ($ext_if) to any
+pass out quick tagged NAT
+pass in quick on pppx tag L2TP
+pass out quick on $int_if tagged L2TP
+pass out quick on $ext_if from ($ext_if) to any
+pass out quick on $int_if from ($int_if) to any
+pass out quick on $vagrant_if from ($vagrant_if) to any
 __EOF__
 
 pfctl -f /etc/pf.conf
